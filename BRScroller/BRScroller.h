c6421022bf859a5cce1bacdfebef3bab0bd8e292
Matt Magoffin <git+matt@msqr.us>
2014-08-12 13:59:53 +1200

    Remove non-core imports from BRScroller.h to support Core CocoaPods spec.

--- BRScroller/BRScroller.h
+++ BRScroller/BRScroller.h
@@ -6,12 +6,7 @@
 //  Copyright (c) 2013 Blue Rocket. Distributable under the terms of the Apache License, Version 2.0.
 //
 
-#import <BRScroller/BRAsyncImageView.h>
-#import <BRScroller/BRCenteringScrollView.h>
-#import <BRScroller/BRImagePreviewLayerView.h>
-#import <BRScroller/BRPreviewLayerView.h>
 #import <BRScroller/BRScrollerDelegate.h>
 #import <BRScroller/BRScrollerUtilities.h>
 #import <BRScroller/BRScrollerView.h>
 #import <BRScroller/BRScrollViewDelegate.h>
-#import <BRScroller/BRZoomingImageView.h>
