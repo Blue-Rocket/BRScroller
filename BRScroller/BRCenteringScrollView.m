588aed8c9d304c7086d7377b48be9ecb94de4d68
Matt Magoffin <git+matt@msqr.us>
2015-03-26 15:18:57 +1300

    Updates for better rotation support on iOS 8. Not completely satisfied with this, only because not as nice looking as iOS 7, but I'm a bit stumped getting it any better.

--- BRScroller/BRCenteringScrollView.m
+++ BRScroller/BRCenteringScrollView.m
@@ -18,6 +18,7 @@
 	UITapGestureRecognizer *doubleTapRecognizer;
 	CGFloat doubleTapZoomIncrement;
 	CGFloat doubleTapMaxZoomLevel;
+	CGSize managedViewSize;
 }
 
 @synthesize scrollDelegate;
@@ -101,31 +102,6 @@
 
 #pragma mark Layout
 
-- (void)setFrame:(CGRect)frame {
-	// UIScrollView will automatically choose a new contentOffset value when the frame changes,
-	// but this can lead to the wrong desired offset in the case of rotation, where we will
-	// be changing the contentSize to adjust to the new frame size. What we want is for the
-	// visual center of the managed content area (our own view bounds) to remain unchanged, so
-	// the content appears to rotate about the center point.
-	//
-	// So we calculate that visual center before the frame changes, then re-calculate it
-	// after setting the frame to achieve the desired effect.
-
-	const CGSize oldViewSize = self.bounds.size;
-	const CGPoint oldContentOffset = self.contentOffset;
-	const CGSize oldContentSize = self.contentSize;
-	const CGPoint centerCoordinate = CGPointMake((oldContentOffset.x + oldViewSize.width * 0.5) / oldContentSize.width,
-												(oldContentOffset.y + oldViewSize.height * 0.5) / oldContentSize.height);
-	[super setFrame:frame];
-	if ( oldContentOffset.y > 0 ) {
-		[self layoutIfNeeded]; // calling this might adjust the contentSize to fit new view bounds
-		const CGSize newContentSize = self.contentSize;
-		const CGPoint newCenterPoint = CGPointMake(centerCoordinate.x * newContentSize.width, centerCoordinate.y * newContentSize.height);
-		const CGPoint newOffset = CGPointMake(MAX(0.0, newCenterPoint.x - frame.size.width * 0.5), MAX(0.0, newCenterPoint.y - frame.size.height * 0.5));
-		[self setContentOffsetWithoutAnimation:newOffset];
-	}
-}
-
 - (void)setBounds:(CGRect)bounds {
 	// UIScrollView will automatically choose a new contentOffset value when the frame changes,
 	// but this can lead to the wrong desired offset in the case of rotation, where we will
@@ -141,30 +117,37 @@
 	const CGSize oldContentSize = self.contentSize;
 	const CGPoint centerCoordinate = CGPointMake((oldContentOffset.x + oldViewSize.width * 0.5) / oldContentSize.width,
 												 (oldContentOffset.y + oldViewSize.height * 0.5) / oldContentSize.height);
+	const BOOL resize = (CGSizeEqualToSize(oldViewSize, bounds.size) == NO);
+	CGPoint newOffset;
+	CGSize newContentSize;
+	
+	UIView *managedView = [self viewForZoomingInScrollView:self];
+	if ( resize ) {
+		CGSize viewSize = bounds.size;
+		CGSize size = [managedView sizeThatFits:viewSize];
+		CGAffineTransform contentScale = CGAffineTransformMakeScale(self.zoomScale, self.zoomScale);
+		newContentSize = CGSizeApplyAffineTransform(size, contentScale);
+		const CGPoint newCenterPoint = CGPointMake(centerCoordinate.x * newContentSize.width - bounds.size.width * 0.5,
+												   centerCoordinate.y * newContentSize.height - bounds.size.height * 0.5);
+		newOffset = [self centeredOffsetForRequestedOffset:CGPointMake(MAX(0.0, newCenterPoint.x), MAX(0.0, newCenterPoint.y))
+											   contentSize:newContentSize
+												  viewSize:viewSize];
+	}
 
 	[super setBounds:bounds];
-	if ( CGSizeEqualToSize(oldViewSize, bounds.size) == NO ) {
-		[self layoutIfNeeded]; // calling this might adjust the contentSize to fit new view bounds
-		const CGSize newContentSize = self.contentSize;
-		const CGPoint newCenterPoint = CGPointMake(centerCoordinate.x * newContentSize.width, centerCoordinate.y * newContentSize.height);
-		const CGPoint newOffset = CGPointMake(MAX(0.0, newCenterPoint.x - bounds.size.width * 0.5), MAX(0.0, newCenterPoint.y - bounds.size.height * 0.5));
-		[self setContentOffsetWithoutAnimation:newOffset];
+	if ( resize ) {
+		[self layoutIfNeeded];
+		[self setContentOffset:newOffset animated:NO];
 	}
 }
 
-- (void)setContentOffset:(const CGPoint)offset {
-	// When the contentSize is smaller than our view bounds, the following keeps the content centered. Normally
-	// UIScrollView will position the view in the top-left corner. This logic is done here because it applies
-	// also during zooming operations, where UIScrollView adjusts the contentOffset and contentSize.
-	
+- (CGPoint)centeredOffsetForRequestedOffset:(CGPoint)offset contentSize:(CGSize)contentSize viewSize:(CGSize)viewSize {
 	CGPoint newOffset = offset;
 	UIView *managedView = [self viewForZoomingInScrollView:self];
 	if ( managedView != nil ) {
-		CGSize contentSize = self.contentSize;
 		UIEdgeInsets inset = self.contentInset;
 		contentSize.width += inset.left + inset.right;
 		contentSize.height += inset.top + inset.bottom;
-		CGSize viewSize = self.bounds.size;
 		if ( contentSize.width < viewSize.width ) {
 			newOffset.x = -(viewSize.width - contentSize.width) / 2.0;
 		}
@@ -173,56 +156,64 @@
 		}
 		log4Trace(@"Centered offset %@ for size %@", NSStringFromCGPoint(newOffset), NSStringFromCGSize(contentSize));
 	}
+	return newOffset;
+}
+
+- (void)setContentOffset:(const CGPoint)offset {
+	// When the contentSize is smaller than our view bounds, the following keeps the content centered. Normally
+	// UIScrollView will position the view in the top-left corner. This logic is done here because it applies
+	// also during zooming operations, where UIScrollView adjusts the contentOffset and contentSize.
+	
+	CGPoint newOffset = [self centeredOffsetForRequestedOffset:offset contentSize:self.contentSize viewSize:self.bounds.size];
 	log4Trace(@"Changing contentOffset (requested %@) from %@ to %@", NSStringFromCGPoint(offset),
 			  NSStringFromCGPoint(self.contentOffset), NSStringFromCGPoint(newOffset));
 	[super setContentOffset:newOffset];
 }
 
-- (void)setContentOffsetWithoutAnimation:(const CGPoint)offset {
-	const BOOL animationEnabled = [UIView areAnimationsEnabled];
-	if ( animationEnabled ) {
-		[UIView setAnimationsEnabled:NO];
+- (BOOL)adjustContentSize {
+	UIView *managedView = [self viewForZoomingInScrollView:self];
+	const CGSize viewSize = self.bounds.size;
+	const CGSize size = [managedView sizeThatFits:viewSize];
+	const CGAffineTransform contentScale = CGAffineTransformMakeScale(self.zoomScale, self.zoomScale);
+	const CGSize expectedContentSize = CGSizeApplyAffineTransform(size, contentScale);
+	if ( !CGSizeEqualToSize(self.contentSize, expectedContentSize) ) {
+		//ignoreScroll = YES;
+		managedViewSize = size;
+		self.contentSize = expectedContentSize;
+		//ignoreScroll = NO;
+		return YES;
 	}
-	[self setContentOffset:offset];
-	if ( animationEnabled ) {
-		[UIView setAnimationsEnabled:YES];
-	}
+	return NO;
 }
+
 
 - (void)layoutSubviews {
 	[super layoutSubviews];
 	
 	// This method is called after view bounds changes as well as during scrolling/zooming gestures.
-	UIView *managedView = [self viewForZoomingInScrollView:self];
-	if ( self.zooming == NO ) {
-		log4Trace(@"Layout size %@; center = %@", NSStringFromCGSize(managedView.bounds.size), NSStringFromCGPoint(managedView.center));
-		
-		// Here we check for size adjustment of our bounds, and adjust managed size accordingly. Adjusting the view size
-		// means we also adjust our own contentSize to match. Changing contentSize causes UIScrollView's contentOffset
-		// to then not match the relative offset we were in at the old size, so we re-compute a new contentOffset
-		// based on the change in scale of the contentSize.
-		
+	const BOOL resize = [self adjustContentSize];
+	
+	if ( resize ) {
+		UIView *managedView = [self viewForZoomingInScrollView:self];
 		CGSize viewSize = self.bounds.size;
-		CGSize size = [managedView sizeThatFits:viewSize];
-		CGAffineTransform contentScale = CGAffineTransformMakeScale(self.zoomScale, self.zoomScale);
-		CGSize contentSize = CGSizeApplyAffineTransform(size, contentScale);
-		if ( !CGSizeEqualToSize(contentSize, self.contentSize) ) {
-			CGFloat diffScale = size.width / managedView.bounds.size.width;
-			CGPoint contentOffset = self.contentOffset;
-			contentOffset = CGPointApplyAffineTransform(contentOffset, CGAffineTransformMakeScale(diffScale, diffScale));
-			
-			// if contentOffset values < 0, we have adjusted them to be centered... but need to keep this centered at new size now
-			if ( contentOffset.x < 0 ) {
-				contentOffset.x = MAX(0.0, -(viewSize.width - contentSize.width) / 2.0);
-			}
-			if ( contentOffset.y < 0 ) {
-				contentOffset.y = MAX(0.0, -(viewSize.height - contentSize.height) / 2.0);
-			}
-			managedView.bounds = CGRectMake(0, 0, size.width, size.height);
-			managedView.center = CGPointApplyAffineTransform(CGPointMake(size.width / 2.0, size.height / 2.0), contentScale);
-			self.contentSize = contentSize;
-			[self setContentOffsetWithoutAnimation:contentOffset];
+		CGSize contentSize = self.contentSize;
+		CGFloat diffScale = managedViewSize.width / managedView.bounds.size.width;
+		
+		CGPoint contentOffset = self.contentOffset;
+		contentOffset = CGPointApplyAffineTransform(contentOffset, CGAffineTransformMakeScale(diffScale, diffScale));
+
+		// if contentOffset values < 0, we have adjusted them to be centered... but need to keep this centered at new size now
+		if ( contentOffset.x < 0 ) {
+			contentOffset.x = MAX(0.0, -(viewSize.width - contentSize.width) / 2.0);
 		}
+		if ( contentOffset.y < 0 ) {
+			contentOffset.y = MAX(0.0, -(viewSize.height - contentSize.height) / 2.0);
+		}
+		
+		self.contentOffset = contentOffset;
+
+		managedView.bounds = CGRectMake(0, 0, managedViewSize.width, managedViewSize.height);
+		managedView.center = CGPointMake(contentSize.width / 2.0, contentSize.height / 2.0);
 	}
 }
 
