9db5b49befa0b27d4151190500d5df379f039fbf
Matt Magoffin <git+matt@msqr.us>
2015-10-27 09:45:16 +1300

    Update documentation.

--- BRScroller/PDF/BRCachedPreviewPdfPageZoomView.h
+++ BRScroller/PDF/BRCachedPreviewPdfPageZoomView.h
@@ -10,11 +10,26 @@
 
 #import "BRCachedPreviewPdfPageView.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ A zoomable scroll view for displaying a single page from a PDF document.
+ */
 @interface BRCachedPreviewPdfPageZoomView : BRCenteringScrollView
 
+/** The zooming content view to display the PDF page. Defaults to a non-nil value. */
 @property (nonatomic, strong) BRCachedPreviewPdfPageView *pdfView;
 
-// call to reset the view with a new page
+/**
+ Display a page from a PDF document. This will invoke @c updateContentWithPage:atIndex:withKey:
+ on the configured @c pdfView.
+ 
+ @param page      The page to display.
+ @param pageIndex The index of the page being shown.
+ @param key       A unique key for this page.
+ */
 - (void)setPage:(CGPDFPageRef)page forIndex:(NSUInteger)pageIndex withKey:(NSString *)key;
 
 @end
+
+NS_ASSUME_NONNULL_END
